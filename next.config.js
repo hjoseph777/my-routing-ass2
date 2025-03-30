564da9522ac76c14d8e9f0718551be32cd2d17c7
H Joseph <hjoseph777@gmail.com>
2025-03-30 04:26:59 -0400

    Configure for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/my-routing-ass2',
+  assetPrefix: '/my-routing-ass2/',
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
